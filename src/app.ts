060adfe2dd390268a247f75de25827e547b7045f
twerno <twerno@gmail.com>
2020-06-14 15:20:56 +0200

    [FIX] concurency

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-// import { run } from './dataProvider/DataProviderRunner';
+import ProvideOfferTask1 from './dataProvider/ProvideOfferTask1';
 import { NovumDataProvider } from './inwestycje/Novum/NovumDataProvider';
 import { OstojaDataProvider } from "./inwestycje/Ostoja/OstojaDataProvider";
-import { saveFile } from "./utils/FileSave";
 import { AsyncTaskRunner } from './utils/asyncTask/AsyncTaskRunner';
-import ProvideOfferTask1 from './dataProvider/ProvideOfferTask1';
 
 console.log('start');
 
@@ -14,22 +12,8 @@ AsyncTaskRunner(
         new ProvideOfferTask1(OstojaDataProvider, 1),
         new ProvideOfferTask1(NovumDataProvider, 2),
     ],
-    {}, errors)
+    {
+        concurency: 10
+    }, errors)
     .catch(err => console.error(err, errors))
     .then(v => console.log('done', v));
-
-// run(OstojaDataProvider)
-//     .then(result => {
-//         saveFile(`${result.dataProvider.inwestycjaId}.txt`, JSON.stringify(result) + '\n');
-//         return result;
-//     })
-//     .then(result => console.log('end', result.errors?.length > 0 ? result.errors : 'bez błędów'))
-//     .catch(console.error);
-
-// run(NovumDataProvider)
-//     .then(result => {
-//         saveFile(`${result.dataProvider.inwestycjaId}.txt`, JSON.stringify(result) + '\n');
-//         return result;
-//     })
-//     .then(result => console.log('end', result.errors?.length > 0 ? result.errors : 'bez błędów'))
-//     .catch(console.error);
--- src/utils/asyncTask/AsyncTaskRunner.ts
+++ src/utils/asyncTask/AsyncTaskRunner.ts
@@ -5,63 +5,88 @@ export interface IAsyncTaskRunnerConfig {
     concurency?: number;
 }
 
-export const AsyncTaskRunner = async (tasks: IAsyncTask[], config?: IAsyncTaskRunnerConfig, errors?: any[]) => {
+export const AsyncTaskRunner = (tasks: IAsyncTask[], config?: IAsyncTaskRunnerConfig, errors?: any[]) => {
 
     return new Promise<void>((resolve, reject) => {
 
-        let runningTasks: number = 0;
+        let ref = { runningTasks: 0 };
 
         if (!tasks || tasks.length === 0) {
             reject('brak tasków');
         }
 
-        const getNextTask = (): IAsyncTask | null => {
-            let nextTask: IAsyncTask | null = null;
-            let nextTaskIdx = -1;
+        const taskLimit = config?.concurency || 5;
 
-            tasks.reverse().forEach((task, idx) => {
-                if ((task.priority || 0) > (nextTask?.priority || -1)) {
-                    nextTask = task;
-                    nextTaskIdx = idx;
-                }
-            });
+        runTasks(tasks, taskLimit, ref, errors || [], resolve);
+    });
 
-            if (nextTaskIdx > -1) {
-                tasks.splice(nextTaskIdx, 1);
-            }
+}
 
-            return nextTask;
+function getNextTask(tasks: IAsyncTask[]): IAsyncTask | null {
+    let nextTask: IAsyncTask | null = null;
+    let nextTaskIdx = -1;
+
+    for (let i = tasks.length - 1; i >= 0; i--) {
+        const task = tasks[i];
+        if ((task.priority || 0) > (nextTask?.priority || -1)) {
+            nextTask = task;
+            nextTaskIdx = i;
         }
+    }
 
-        const runNextTask = async () => {
-            if (tasks.length === 0) {
-                if (runningTasks === 0) {
-                    resolve();
-                }
-                return;
-            }
+    if (nextTaskIdx > -1) {
+        tasks.splice(nextTaskIdx, 1);
+    }
 
-            const task = getNextTask();
-            try {
-                runningTasks++;
-                console.log('run task', task);
-                const result = await task?.run(errors || []) || [];
-                tasks.push.apply(tasks, result instanceof Array ? result : [result]);
-            }
-            catch (err) {
-                TaskHelper.silentErrorReporter(errors || [], { method: 'runNextTask', task })(err);
-            }
-            finally {
-                console.log('task finished', task);
-                runningTasks--;
-                Promise.resolve()
-                    .then(runNextTask);
-            }
+    return nextTask;
+}
+
+async function runNextTask(
+    tasks: IAsyncTask[],
+    ref: { runningTasks: number },
+    errors: any[],
+    callback: () => void
+) {
+    const task = getNextTask(tasks);
+    try {
+        ref.runningTasks++;
+        console.log('run task', (task as any)?.__proto__?.constructor?.name);
+        console.log(ref.runningTasks, tasks.length);
+        const result = await task?.run(errors || []) || [];
+        tasks.push.apply(tasks, result instanceof Array ? result : [result]);
+    }
+    catch (err) {
+        TaskHelper.silentErrorReporter(errors || [], { method: 'runNextTask', task })(err);
+    }
+    finally {
+        console.log('task finished', (task as any)?.__proto__?.constructor?.name);
+        ref.runningTasks--;
+        callback();
+    }
+}
+
+function runTasks(
+    tasks: IAsyncTask[],
+    taskLimit: number,
+    ref: { runningTasks: number },
+    errors: any[],
+    callback: () => void
+) {
+    if (ref.runningTasks >= taskLimit) {
+        return;
+    }
+
+    if (tasks.length === 0) {
+        if (ref.runningTasks === 0) {
+            callback();
         }
+        return;
+    }
 
-        for (let i = 0; i < (config?.concurency || 5); i++) {
-            runNextTask();
-        }
-    })
+    const taskToBeExecuted = Math.min(taskLimit - ref.runningTasks, tasks.length);
+
+    for (let i = 0; i < taskToBeExecuted; i++) {
+        runNextTask(tasks, ref, errors, () => runTasks(tasks, taskLimit, ref, errors, callback));
+    }
 
 }
